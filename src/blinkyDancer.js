a21b3efc7a6faab22233966f72de9da2fec2d2de
HarleyKwyn <kwyn.meagher@gmail.com>
2014-01-13 15:16:06 -0800

    Dancer constructor created.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,20 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps){
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+  var blinkyDancer = new Dancer(top, left, timeBetweenSteps);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
-  var oldStep = blinkyDancer.step;
+  blinkyDancer.oldStep = blinkyDancer.step;
 
   blinkyDancer.step = function(){
     // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
+    blinkyDancer.oldStep();
 
     /* toggle() is a jQuery method to show/hide the <span> tag.
      * See http://api.jquery.com/category/effects/ for this and
      * other effects you can use on a jQuery-wrapped html tag. */
     blinkyDancer.$node.toggle();
   };
+  blinkyDancer.step();
 
   return blinkyDancer;
 };
--- src/dancer.js
+++ src/dancer.js
@@ -1,32 +1,29 @@
 // Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps){
-
-  var dancer = {};
+var Dancer = function(top, left, timeBetweenSteps){
 
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
-
-  dancer.step = function(){
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
-  };
-  dancer.step();
-
-  dancer.setPosition = function(top, left){
-    /* Use css top and left properties to position our <span> tag
-     * where it belongs on the page. See http://api.jquery.com/css/
-     */
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    dancer.$node.css(styleSettings);
-  };
+  this.$node = $('<span class="dancer"></span>');
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
-
-  return dancer;
+  this.setPosition(top, left);
+  this.timeBetweenSteps = timeBetweenSteps;
 };
+
+Dancer.prototype.step = function(){
+  // the basic dancer doesn't do anything interesting at all on each step,
+  // it just schedules the next step
+  setTimeout(this.step, this.timeBetweenSteps);
+};
+
+Dancer.prototype.setPosition = function(top, left){
+  /* Use css top and left properties to position our <span> tag
+   * where it belongs on the page. See http://api.jquery.com/css/
+   */
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  this.$node.css(styleSettings);
+};
+
